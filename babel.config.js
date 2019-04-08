f8097a91ac422e0a8e44e5b48f11bae242ea0766
Francesc <francescq@gmail.com>
2019-04-08 16:25:36 +0200

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react'],
+<<<<<<< HEAD
     plugins: ['@babel/plugin-proposal-class-properties'],
+=======
+>>>>>>> initial commit
 };
